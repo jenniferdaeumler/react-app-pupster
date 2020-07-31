fcbdd88af5e76d8358f3e3d829ddfa598477b66d
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-31 14:07:25 -0500

    Updates

---
+++ precache-manifest.6f2bda85d6f255cd3fa7028cbe3637e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3afb3a45b392e9633409da82733dc3",
+    "url": "/react-app-pupster/index.html"
+  },
+  {
+    "revision": "dc89fdf74c2ea2ecb244",
+    "url": "/react-app-pupster/static/js/2.51d4356b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-pupster/static/js/2.51d4356b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8097a89a4d0d2cae8e",
+    "url": "/react-app-pupster/static/js/main.245971b2.chunk.js"
+  },
+  {
+    "revision": "19ea3a67068ac9da4e05",
+    "url": "/react-app-pupster/static/js/runtime-main.1c248239.js"
+  }
+]);
